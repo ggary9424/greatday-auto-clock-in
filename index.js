33958a9a47afdbf2208a84e3e5322ef36820aa31
Gary Chen <ggary9424@gmail.com>
2021-11-20 21:34:48 +0800

    New endpoint /attendance-list

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const { version: appVersion } = require('./package.json');
-const helmet = require('helmet');
+const { version: appVersion } = require("./package.json");
+const helmet = require("helmet");
+const Config = require("config");
+const _ = require("lodash");
 
-const SERVER_PORT = process.env.PORT || 5000;
+const greatday = require("./lib/greatday");
 
-console.log(SERVER_PORT)
+const SERVER_PORT = Config.get("SERVER_PORT");
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.set('port', SERVER_PORT);
+app.set("port", SERVER_PORT);
 
 app.use(helmet.hidePoweredBy());
 app.use(helmet.hsts());
@@ -17,23 +19,45 @@ app.use(helmet.noSniff());
 app.use(helmet.referrerPolicy());
 app.use(helmet.xssFilter());
 
-app.get('/', (req, res) => res.status(200).json({
-  appVersion,
-}));
+app.get("/", (req, res) =>
+  res.status(200).json({
+    appVersion,
+  })
+);
 
-app.get('*', (req, res) => {
+app.get("/attendance-list", async (req, res) => {
+  const attendances = await greatday.getAttendanceList();
+  const transformStatus = (otherStatus) => {
+    if (otherStatus.includes("ABS")) {
+      return "缺席";
+    }
+    return "未知";
+  };
+
+  return res.status(200).json({
+    attendance_list: _.map(attendances, (attendance) => ({
+      time: attendance.time,
+      status: transformStatus(attendance.otherStatus),
+    })),
+  });
+});
+
+app.get("*", (req, res) => {
   res.status(404).json(null);
 });
 
 // Common error handling
 app.use((error, req, res, next) => {
   return res.status(500).json({
-    errors: [{ msg: 'An unexpected error occurred. Please contact us.', code: 'unknown' }],
+    errors: [
+      {
+        msg: "An unexpected error occurred. Please contact us.",
+        code: "unknown",
+      },
+    ],
   });
 });
 
-const server = app.listen(
-  app.get('port'), () => {
-    console.log('running on port', app.get('port'));
-  },
-);
+const server = app.listen(app.get("port"), () => {
+  console.log("running on port", app.get("port"));
+});
